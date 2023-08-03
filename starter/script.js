778d32fd4603044d6e126eb002238466e9414a57
hugo pereira <Hugopereira8814@gmail.com>
2023-08-03 20:49:27 +0100

    done

---
+++ starter/script.js
@@ -0,0 +1,159 @@
+"use strict";
+
+// let hasDriversLincense = false;
+// const passTest = true;
+
+// if (passTest) hasDriverLincense = true;
+// if (hasDriversLincense) console.log("i can drive");
+
+// //const private = 32;
+// //const if = 23
+
+// function logger() {
+//   console.log("my name is hugo");
+// }
+// calling //running // invoking the function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges}
+//   oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(3, 4);
+// console.log(appleOrangeJuice);
+
+// function describeCountry(country, population, capitalCity) {
+//   const facts = `${country} has ${population} people and its capital city is ${capitalCity}`;
+//   return facts;
+// }
+// const uk = describeCountry("england", "74 million", "london");
+// console.log(uk);
+// const usa = describeCountry("USA", "314 million", "washinton DC");
+// console.log(usa);
+// const br = describeCountry("brazil", "224 million", "brazila");
+// console.log(br);
+
+//fuction del
+
+// function calcAge1(birthYear) {
+//   return 2030 - birthYear;
+// }
+
+// const age1 = calcAge1(2003);
+// console.log(age1);
+
+// //funtion expression
+// const calcAge2 = function (birthYear) {
+//   return 2030 - birthYear;
+// };
+// const Age2 = calcAge2(2003);
+// console.log(Age2);
+
+// function percentageOfWorld1(pop) {
+//   return (pop / 7900) * 100;
+// }
+// const pop1 = percentageOfWorld1(1100);
+// console.log(pop1);
+// const pop2 = percentageOfWorld1(100);
+// console.log(pop2);
+// const pop3 = percentageOfWorld1(500);
+// console.log(pop3);
+
+// const popcalc = function (pop21) {
+//   return (pop21 / 7900) * 100;
+// };
+// const pop4 = popcalc(1100);
+// console.log(pop4);
+// const pop5 = popcalc(700);
+// console.log(pop5);
+// const pop6 = popcalc(2300);
+// console.log(pop6);
+
+// arrow function
+// const calcAge3 = (birthYear) => 2030 - birthYear;
+// const age3 = calcAge3(2003);
+// console.log(age3);
+
+// const yearTilRetire = (birthYear, firstName) => {
+//   const age = 2023 - birthYear;
+//   const retire = 65 - age;
+//   //return retire;
+//   return `${firstName} retires in ${retire} years`;
+// };
+// console.log(yearTilRetire(2003, "hugo"));
+// console.log(yearTilRetire(1984, "gisa"));
+
+// const popcalc = (pop21) => {
+//   return (pop21 / 7900) * 100;
+// };
+// const pop4 = popcalc(1100);
+// console.log(pop4);
+// const pop5 = popcalc(700);
+// console.log(pop5);
+// const pop6 = popcalc(2300);
+// console.log(pop6);
+
+// function cutPieces(fruit) {
+//   return fruit * 6;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applesPieces = cutPieces(apples);
+//   const orangePieces = cutPieces(oranges);
+//   const juice = `Juice with ${applesPieces} apples pieces and ${orangePieces}
+//       oranges pieces.`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 4));
+
+// const pop1 = function (pop) {
+//   return (pop / 7900) * 100;
+// };
+
+// const describePop = function (country, pop) {
+//   const per = pop1(pop);
+//   const des = `${country} has ${pop} million people, which is about ${per}% of the world.`;
+//   console.log(des);
+// };
+
+// describePop("UK", 74);
+
+// const pop = function (pop) {
+//   return (pop / 7900) * 100;
+// };
+
+// const describePop1 = function (country, pop) {
+//   const per1 = pop(pop);
+//   const des1 = `${country} has ${pop} million
+//     people, which is about ${per}% of the world.`;
+//   console.log(des);
+// };
+// describePop1("Portugal", 10);
+
+const calcAge = function (birthYear) {
+  return 2023 - birthYear;
+};
+
+const yearTilRetire = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retire = 65 - age;
+  if (retire > 0) {
+    return retire;
+    console.log(`${firstName} retires in ${retire} years`);
+  } else {
+    return -1;
+    console.log`${firstName} has already retired`;
+  }
+
+  //return `${firstName} retires in ${retire} years`;
+};
+
+console.log(yearTilRetire(2003, "hugo"));
+console.log(yearTilRetire(1942, "mike"));
