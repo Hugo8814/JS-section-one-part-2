15e792efb3de26dff63adc883e88579d04199ff9
hugo pereira <Hugopereira8814@gmail.com>
2023-08-17 07:40:22 +0100

    done, next

--- starter/script.js
+++ starter/script.js
@@ -442,16 +442,16 @@ const jonas = [
 // console.log(ages);
 
 // continue and break
-console.log("--- ONLY STRINGS ---");
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] !== "string") continue;
+// console.log("--- ONLY STRINGS ---");
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] !== "string") continue;
 
-  console.log(jonas[i], typeof jonas[i]);
-}
+//   console.log(jonas[i], typeof jonas[i]);
+// }
 
-console.log("--- BREAK WITH NUMBER ---");
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] === "number") break;
+// console.log("--- BREAK WITH NUMBER ---");
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] === "number") break;
 
-  console.log(jonas[i], typeof jonas[i]);
-}
+//   console.log(jonas[i], typeof jonas[i]);
+// }
