1a2a5b2da702a3f3c3396bab816b64cdac6296a4
hugo pereira <Hugopereira8814@gmail.com>
2023-08-18 18:58:59 +0100

    done, next

--- starter/script.js
+++ starter/script.js
@@ -455,3 +455,10 @@ const jonas = [
 
 //   console.log(jonas[i], typeof jonas[i]);
 // }
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
